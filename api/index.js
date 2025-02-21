af5ef87fdb35e6f196e024b3259282460b627dc9
Onome9 <osahunmenonome980@gmail.com>
2025-02-21 18:16:47 +0000

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -13,12 +14,13 @@ if (!process.env.MONGO) {
 
 
 mongoose
-.connect(process.env.MONGO)
+.connect(process.env.MONGO, {useNewUrlParser: true,
+    useUnifiedTopology: true})
 .then(() => {
     console.log('MongoDb is connected');
 })
 .catch((err) => {
-    console.log(err);
+    console.log('Mongodb failed', err);
 });
 
 const app = express ();
@@ -26,3 +28,6 @@ const app = express ();
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 });
+
+
+app.use('/api/user', userRoutes);
